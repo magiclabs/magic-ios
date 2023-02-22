14247a2f64c0851f04f696cf2911f923dfd58ddc
Arian Flores <arian.flores@magic.link>
2023-02-21 16:19:24 -0800

    Adds RecoverAccountConfiguration

--- Sources/MagicSDK/Modules/User/UserModule.swift
+++ Sources/MagicSDK/Modules/User/UserModule.swift
@@ -138,14 +138,16 @@ public class UserModule: BaseModule {
     /**
         recoverAccount
      */
-    public func recoverAccount(response: @escaping Web3ResponseCompletion<String>) {
-        let request = BasicRPCRequest(method: UserMethod.magic_auth_recover_account.rawValue, params: [])
-        self.provider.send(request: request, response: response)
+    public func recoverAccount(_ configuration: RecoverAccountConfiguration, response: @escaping Web3ResponseCompletion<Bool>) {
+        
+        let request = RPCRequest<[RecoverAccountConfiguration]>(method: UserMethod.magic_auth_recover_account.rawValue, params: [configuration])
+        
+        return self.provider.send(request: request, response: response)
     }
     
-    public func recoverAccount() -> Promise<String> {
+    public func recoverAccount(_ configuration: RecoverAccountConfiguration) -> Promise<Bool> {
         return Promise { resolver in
-            showSettings(response: promiseResolver(resolver))
+            recoverAccount(configuration, response: promiseResolver(resolver))
         }
     }
 }
